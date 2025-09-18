e51188116f390317c47b0d2352c84c2776c63a69
Priyansh <21f1004415@ds.study.iitm.ac.in>
2025-09-18 16:13:29 +0530

    Fix duplicate http require declaration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const express = require('express');
-const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
